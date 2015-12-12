fac2bf7e08898fe8734adae099b59f220ec877ab
marina <m.chernyavska@gmail.com>
2015-12-12 23:21:43 +0100

    adding goodreads - partial commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,51 @@
 from flask import Flask
+from flask_oauthlib.client import OAuth
+from celery import Celery
+import os
+import pymongo
+
+GITHUB_CLIENT_ID = os.environ.get("GITHUB_CLIENT_ID")
+GITHUB_CLIENT_SECRET = os.environ.get("GITHUB_SECRET_ID")
+GOODREADS_KEY_ID = os.environ.get("GOODREADS_KEY_ID")
+GOODREADS_SECRET_ID = os.environ.get("GOODREADS_SECRET_ID")
 
 app = Flask(__name__)
 app.config.from_object('config')
+
+oauth = OAuth(app)
+github_auth = oauth.remote_app(
+    'github',
+    consumer_key=GITHUB_CLIENT_ID,
+    consumer_secret=GITHUB_CLIENT_SECRET,
+    request_token_params={'scope': 'user:email'},
+    base_url='https://api.github.com/',
+    request_token_url=None,
+    access_token_method='POST',
+    access_token_url='https://github.com/login/oauth/access_token',
+    authorize_url='https://github.com/login/oauth/authorize'
+)
+goodreads_auth = oauth.remote_app(
+    'goodreads',
+    consumer_key=GOODREADS_KEY_ID,
+    consumer_secret=GOODREADS_SECRET_ID,
+    request_token_url='http://www.goodreads.com/oauth/request_token',
+    authorize_url='http://www.goodreads.com/oauth/authorize',
+    access_token_url='http://www.goodreads.com/oauth/access_token',
+    access_token_method='POST',
+    base_url='http://www.goodreads.com/'
+)
+
+MONGO_URI = "mongodb://localhost:27017"
+MONGO_DB = "hacktus"
+
+
+def connect_mongo():
+    return pymongo.MongoClient(MONGO_URI)[MONGO_DB]
+
+
+db = connect_mongo()
+
+BROKER_URL = 'redis://localhost:6379/0'
+celery = Celery('tasks', broker=BROKER_URL)
 
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,47 +1,111 @@
-from app import app
-from .forms import LoginForm
-from flask import render_template, flash, redirect
-import requests
-import json
+from app import app, github_auth, goodreads_auth, db
+from flask import render_template, flash, redirect, request, g, session, url_for, jsonify
+from app.github_task import GithubProvider, get_stats_for_day as github_stats
+from app.goodreads_task import GoodreadsProvider, get_stats_for_day as goodreads_stats
+from goodreads.session import GoodreadsSession
+import webbrowser
+import datetime, os
 
-GITHUB_API = 'https://api.github.com'
-gh = {
-    'login': None,
-    'password': None
-}
+
+@app.before_request
+def before_request():
+    g.user = None
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'Marina'}  # fake user
+    if 'github_token' in session:
+        user = github_auth.get('user')
+        return jsonify(user.data)
+    else:
+        user = None
     return render_template('index.html',
                            title='Home',
                            user=user)
 
-@app.route('/signin')
-def signin():
-    url = GITHUB_API + '/authorizations'
-    payload = {
-        'note': 'temporary auth'
-    }
-    res = requests.post(
-        url,
-        auth = (gh['login'], gh['password']),
-        data = json.dumps(payload),
-    )
-    return res.text
-    # return render_template('signin.html',
-    #                        title='Home')
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/gh_callback')
+def authorized():
+    resp = github_auth.authorized_response()
+    if resp is None:
+        return 'Access denied'
+    session['github_token'] = (resp['access_token'], '')
+    user = github_auth.get('user')
+    print("Authorized GitHub, token is {}".format(resp['access_token']))
+    return jsonify(user.data)
+
+
+@app.route('/goodreads_callback')
+def authorized_goodreads():
+    if request.args['oauth_token'] == None:
+        return 'Access denied'
+    session['goodreads_token'] = request.args['oauth_token']
+    print("Authorized Goodreads, token is {}".format(session['goodreads_token']))
+    return jsonify(request.args)
+
+
+@app.route('/login')
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        gh['login'] = form.gh_login.data
-        gh['password'] = form.gh_password.data
-        flash('Login requested for GitHub="%s", remember_me=%s' %
-              (form.gh_login.data, str(form.remember_me.data)))
-        return redirect('/signin')
-    return render_template('login.html',
-                           title='Sign In',
-                           form=form)
+    if session.get('user_id', None) is None:
+        return github_auth.authorize(
+            callback=url_for('authorized', _external=True))
+    else:
+        return "Already logged in"
+
+
+@github_auth.tokengetter
+def get_github_oauth_token():
+    return session.get('github_token')
+
+
+@app.route('/stats')
+def stats():
+    token = get_github_oauth_token()[0]
+    result = []
+    now = datetime.date.today().toordinal()
+    for x in range(7):
+        dt = now - x
+        stats = db.github.by_day.find_one({'dt': dt})
+        if stats:
+            stats = stats['stats']
+        else:
+            stats = {"msg": "IN PROGRESS"}
+            github_stats.delay(token, datetime.datetime.fromordinal(dt))
+
+        result.append(stats)
+
+    return jsonify({'result': result})
+
+@goodreads_auth.tokengetter
+def get_goodreads_oauth_token():
+    return session.get('goodreads_token')
+
+
+@app.route('/goodreads')
+def goodreads():
+    if 'goodreads_token' in session:
+        gp = GoodreadsProvider(goodreads_auth.consumer_key, goodreads_auth.consumer_secret)
+        gc = gp.client
+        gc.authenticate = authenticate
+        gc.authenticate(gc, session['goodreads_token'])
+        gc.user()
+        print(gc.session.access_token)
+        print(gc.session.access_token_secret)
+        return jsonify({'token': session['goodreads_token']})
+
+    return goodreads_auth.authorize(
+            callback=url_for('authorized_goodreads', _external=True))
+
+
+def authenticate(self, access_token=None, access_token_secret=None):
+    """Authenticate client to query requiring authorization"""
+    self.session = GoodreadsSession(self.client_key, self.client_secret,
+                                    access_token, access_token_secret)
+    if access_token and access_token_secret:
+        self.session.oauth_resume()
+    else:
+        url = self.session.oauth_init()
+        webbrowser.open(url)
+        self.session.oauth_finalize()
+        return (self.session.access_token, self.session.access_token_secret)
